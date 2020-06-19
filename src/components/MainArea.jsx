9b66b33da8619a187dcc97f0ca29b60e58487a10
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-06-19 23:39:14 +0300

    Add Column component
    Co-authored-by: Chalarangelo <chalarangelo@gmail.com>

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Column from './Column';
 
 const MainArea = () => {
   return (
     <main>
-      <div className="main-area-content">Main content</div>
+      <div className="main-area-content">
+        <Column
+          title="To Do"
+          cards={[
+            { title: 'Hallo', id: 0 },
+            { title: 'Hola', id: 1 },
+            { title: 'Gulp', id: 2 }
+          ]}
+        />
+        <Column
+          title="In Progress"
+          cards={[
+            { title: 'Start', id: 3 },
+            { title: 'Stop', id: 4 }
+          ]}
+        />
+        <Column
+          title="Done"
+          cards={[
+            { title: 'Tada', id: 5 }
+          ]}
+        />
+      </div>
     </main>
   );
 };
