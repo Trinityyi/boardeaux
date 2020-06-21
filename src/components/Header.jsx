f5178df82590acfc28d682d711b6aca7dfd5edeb
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-06-21 20:53:08 +0300

    Connect Header to redux, enable board name edit
    Co-authored-by: Chalarangelo <chalarangelo@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import EditableText from './EditableText';
+import {setBoardName} from '../store';
 import logo from '../assets/logo.png';
 
-const Header = () => {
+const Header = ({
+  name,
+  setBoardName
+}) => {
   return (
     <header>
       <img src={logo} alt="Boardeaux" className="logo"/>
+      <h2 className="board-name">
+        <i className="icon icon-layout" />
+        <EditableText
+          id="board-name"
+          name="board-name"
+          value={name}
+          onChange={setBoardName}
+        />
+      </h2>
       <div className="controls">
         <i className="icon icon-search" />
         <input
@@ -19,7 +35,16 @@ const Header = () => {
 };
 
 Header.propTypes = {
-
+  name: PropTypes.string.isRequired,
+  setBoardName: PropTypes.func.isRequired
 };
 
-export default Header;
+const mapStateToProps = state => {
+  return {name: state.name};
+};
+
+const mapDispatchToProps = dispatch => {
+  return { setBoardName: bindActionCreators(setBoardName, dispatch)};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
