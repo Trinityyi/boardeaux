03755f80baa2725a9a231c5e9091c37d7c622a8a
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-06-28 17:11:37 +0300

    Connect mainArea to redux state
    Dynamically retrieve cards and columns from state

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,39 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import {createCard, createColumn} from '../store';
 import Column from './Column';
 
-const MainArea = () => {
+const MainArea = ({
+  cards,
+  createCard,
+  columns,
+  createColumn
+}) => {
+  useEffect(() => {
+    const columnIds = [
+      createColumn({ title: 'To Do' }),
+      createColumn({ title: 'In Progress' }),
+      createColumn({ title: 'Done' })
+    ].map(x => x.id);
+    createCard({ title: 'Hallo' }, columnIds[0]);
+    createCard({ title: 'Hola' }, columnIds[0]);
+    createCard({ title: 'Gulp' }, columnIds[0]);
+    createCard({ title: 'Start' }, columnIds[1]);
+    createCard({ title: 'Stop' }, columnIds[1]);
+    createCard({ title: 'Tada' }, columnIds[2]);
+  }, [createCard, createColumn]);
   return (
     <main>
       <div className="main-area-content">
-        <Column
-          title="To Do"
-          cards={[
-            { title: 'Hallo', id: 0 },
-            { title: 'Hola', id: 1 },
-            { title: 'Gulp', id: 2 }
-          ]}
-        />
-        <Column
-          title="In Progress"
-          cards={[
-            { title: 'Start', id: 3 },
-            { title: 'Stop', id: 4 }
-          ]}
-        />
-        <Column
-          title="Done"
-          cards={[
-            { title: 'Tada', id: 5 }
-          ]}
-        />
+        {Object.values(columns).map(col => (
+          <Column
+            key={col.id}
+            title={col.title}
+            cards={col.cardIds.map(cId => cards[cId])}
+          />
+        ))}
       </div>
     </main>
   );
 };
 
 MainArea.propTypes = {
-
+  cards: PropTypes.shape({}).isRequired,
+  columns: PropTypes.shape({}).isRequired,
+  createCard: PropTypes.func.isRequired,
+  createColumn: PropTypes.func.isRequired
 };
 
-export default MainArea;
+const mapStateToProps = state => {
+  return {
+    cards: state.cards,
+    columns: state.columns
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createCard: bindActionCreators(createCard, dispatch),
+    createColumn: bindActionCreators(createColumn, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainArea);
