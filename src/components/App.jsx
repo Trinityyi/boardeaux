a73f7bc74abf9df207d68077ccc28899ef1f0e64
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-06-28 17:09:42 +0300

    Add redux-thunk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import MainArea from './MainArea';
 import Header from './Header';
 import store from '../store';
 
 const App = () => {
   return (
-    <Provider store={createStore(store)}>
+    <Provider store={createStore(store, applyMiddleware(thunk))}>
       <Header />
       <MainArea />
     </Provider>
