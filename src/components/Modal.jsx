ae4430f164b50a378d81e29bb83cbe704d7bd273
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-07-13 16:53:36 +0300

    fixup! Add Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import combineClassNames from '@chalarangelo/combine-class-names';
+import useClickOutside from '../hooks/useClickOutside';
 
 const Modal = ({
   children,
   id,
+  onClose,
   className = '',
   container
 }) => {
+  const clickRef = useRef();
+  useClickOutside(clickRef, () => {
+    onClose();
+  });
+
   return ReactDOM.createPortal(
     <div className="modal-wrapper">
       <div
         className={combineClassNames`modal-content ${className}`}
         id={id}
+        ref={clickRef}
       >
         {children}
       </div>
@@ -25,6 +33,7 @@ const Modal = ({
 Modal.propTypes = {
   children: PropTypes.node,
   id: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
   className: PropTypes.string,
   container: PropTypes.node.isRequired
 };
