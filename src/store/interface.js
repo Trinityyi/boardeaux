0bd03a959e8f1c5b3bafcbc665968edd7b1bee93
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-08-02 14:10:08 +0300

    Add main menu modal toggle to redux state

--- src/store/interface.js
+++ src/store/interface.js
@@ -1,9 +1,11 @@
 export const initialState = {
-  cardModalId: null
+  cardModalId: null,
+  isMainMenuOpen: false
 };
 
 export const actionTypes = {
-  SET_CARD_MODAL_ID: 'SET_CARD_MODAL_ID'
+  SET_CARD_MODAL_ID: 'SET_CARD_MODAL_ID',
+  SET_MAIN_MENU_OPEN: 'SET_MAIN_MENU_OPEN'
 };
 
 const reducer = (state = initialState, action) => {
@@ -12,6 +14,11 @@ const reducer = (state = initialState, action) => {
     return {
       ...state,
       cardModalId: action.cardId
+    };
+  case actionTypes.SET_MAIN_MENU_OPEN:
+    return {
+      ...state,
+      isMainMenuOpen: action.isOpen
     };
   default:
     return state;
@@ -23,6 +30,12 @@ export const actions = {
     return {
       type: actionTypes.SET_CARD_MODAL_ID,
       cardId
+    };
+  },
+  setMainMenuOpen: isOpen => {
+    return {
+      type: actionTypes.SET_MAIN_MENU_OPEN,
+      isOpen
     };
   }
 };
