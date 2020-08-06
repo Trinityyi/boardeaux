f2c879377ecd248684cb56012fdfa73fb8745930
Chalarangelo <chalarangelo@gmail.com>
2020-08-06 16:11:46 +0300

    Add tests for MenuPanel

---
+++ src/test/components/MenuPanel.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderConnected from '../utils/renderConnected';
+import MenuPanel from '../../components/MenuPanel';
+
+describe('<MenuPanel/>', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const utils = renderConnected(
+      <MenuPanel />,
+      {
+        initialState: {
+          interface: {
+            isMainMenuOpen: true
+          }
+        }
+      }
+    );
+    wrapper = utils.container;
+  });
+
+  it('renders an appropriate wrapper', () => {
+    expect(wrapper.querySelector('.main-menu-wrapper')).toBeInTheDocument();
+  });
+
+  it('renders visible', () => {
+    expect(wrapper.querySelector('.main-menu-wrapper.hidden')).not.toBeInTheDocument();
+  });
+
+  it('renders an appropriate inner wrapper', () => {
+    expect(wrapper.querySelector('.main-menu-panel')).toBeInTheDocument();
+  });
+
+  it('renders a close button', () => {
+    expect(wrapper.querySelector('.btn-menu-close')).toBeInTheDocument();
+  });
+
+  describe('when close button licked', () => {
+    beforeEach(() => {
+      fireEvent.click(wrapper.querySelector('.btn-menu-close'));
+    });
+
+    it('renders hidden', () => {
+      expect(wrapper.querySelector('.main-menu-wrapper.hidden')).toBeInTheDocument();
+    });
+  });
+});
