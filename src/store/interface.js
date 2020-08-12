0a2ea9f39f17d98070e3f45efa44814b5332da0f
Chalarangelo <chalarangelo@gmail.com>
2020-08-12 23:35:02 +0300

    Add drag and drop logic to interface redux store

--- src/store/interface.js
+++ src/store/interface.js
@@ -1,11 +1,16 @@
 export const initialState = {
   cardModalId: null,
-  isMainMenuOpen: false
+  isMainMenuOpen: false,
+  hoveredCardId: null,
+  hoveredCardState: false,
+  draggedCard: null
 };
 
 export const actionTypes = {
   SET_CARD_MODAL_ID: 'SET_CARD_MODAL_ID',
-  SET_MAIN_MENU_OPEN: 'SET_MAIN_MENU_OPEN'
+  SET_MAIN_MENU_OPEN: 'SET_MAIN_MENU_OPEN',
+  SET_HOVERED_CARD: 'SET_HOVERED_CARD',
+  SET_DRAGGED_CARD: 'SET_DRAGGED_CARD'
 };
 
 const reducer = (state = initialState, action) => {
@@ -19,6 +24,17 @@ const reducer = (state = initialState, action) => {
     return {
       ...state,
       isMainMenuOpen: action.isOpen
+    };
+  case actionTypes.SET_HOVERED_CARD:
+    return {
+      ...state,
+      hoveredCardId: action.hoveredCardId,
+      hoveredCardState: action.hoveredCardState
+    };
+  case actionTypes.SET_DRAGGED_CARD:
+    return {
+      ...state,
+      draggedCard: action.cardId
     };
   default:
     return state;
@@ -37,7 +53,27 @@ export const actions = {
       type: actionTypes.SET_MAIN_MENU_OPEN,
       isOpen
     };
-  }
+  },
+  setHoveredCard: (id, hoveredState) => {
+    if (hoveredState.position) {
+      return {
+        type: actionTypes.SET_HOVERED_CARD,
+        hoveredCardId: id,
+        hoveredCardState: hoveredState
+      };
+    }
+    return {
+      type: actionTypes.SET_HOVERED_CARD,
+      hoveredCardId: null,
+      hoveredCardState: false
+    };
+  },
+  setDraggedCard: cardId => {
+    return {
+      type: actionTypes.SET_DRAGGED_CARD,
+      cardId
+    };
+  },
 };
 
 export default reducer;
