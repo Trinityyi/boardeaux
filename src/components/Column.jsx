8cc18052aafe8e897a2b088490b35d9ed41c68a9
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-08-12 12:58:21 +0300

    Make column a drop target

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { useDrop } from 'react-dnd';
 import PropTypes from 'prop-types';
 import Card from './Card';
 import AddButton from './AddButton';
 import actions from '../store/actions';
 
-const { createCard } = actions;
+const { createCard, addCardToColumn } = actions;
 
 /**
  * Renders a column on the board, containing the provided cards.
@@ -15,17 +16,29 @@ const Column = ({
   id,
   cards,
   title,
-  createCard
+  createCard,
+  addCardToColumn
 }) => {
+  const [{ isOver, canDrop }, drop] = useDrop({
+    accept: ['card'],
+    canDrop: item => item.sourceColumnId !== id,
+    drop: item => {
+      addCardToColumn(item.id, id);
+    },
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
   return (
     <div className="column">
       <div className="column-title">
         <h3>{title}</h3>
         <button className="btn btn-column-menu icon icon-more-horizontal" />
       </div>
-      <ul className="column-content">
+      <ul className="column-content" ref={drop}>
         {cards.map(card => (
-          <Card key={card.id} card={card}/>
+          <Card key={card.id} card={card} columnId={id}/>
         ))}
       </ul>
       <AddButton
@@ -47,12 +60,14 @@ Column.propTypes = {
     id: PropTypes.string.isRequired
   })).isRequired,
   title: PropTypes.string.isRequired,
-  createCard: PropTypes.func.isRequired
+  createCard: PropTypes.func.isRequired,
+  addCardToColumn: PropTypes.func.isRequired
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    createCard: bindActionCreators(createCard, dispatch)
+    createCard: bindActionCreators(createCard, dispatch),
+    addCardToColumn: bindActionCreators(addCardToColumn, dispatch)
   };
 };
 
