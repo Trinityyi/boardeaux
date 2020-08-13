1cac36fe076d8e0bdafa6543c86dba6dc21d2aa0
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-08-13 19:04:44 +0300

    Add specs for drag and drop to card

--- src/test/components/Card.test.jsx
+++ src/test/components/Card.test.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { waitFor, fireEvent } from '@testing-library/react';
 import renderDndConnected from '../test_utils/renderDndConnected';
 import Card from '../../components/Card';
 
 const noop = () => { };
 
 describe('<Card/>', () => {
-  let wrapper;
+  let wrapper, cardWrapper, card;
 
   beforeEach(() => {
     const utils = renderDndConnected(
@@ -25,5 +26,36 @@ describe('<Card/>', () => {
 
   it('renders an appropriate element', () => {
     expect(wrapper.querySelector('li')).toBeInTheDocument();
+  });
+
+  describe('drag', () => {
+    beforeEach(() => {
+      cardWrapper = wrapper.querySelector('.card-wrapper');
+      card = wrapper.querySelector('.card-wrapper > .card');
+    });
+
+    it('initial opacity is 1', () => {
+      expect(card.style.opacity).toEqual('1');
+    });
+
+    describe('when drag starts', () => {
+      beforeEach(() => {
+        fireEvent.dragStart(cardWrapper);
+      });
+
+      it('opacity is 0', async() => {
+        await waitFor(() => expect(card.style.opacity).toEqual('0'));
+      });
+
+      describe('when drag end', () => {
+        beforeEach(() => {
+          fireEvent.dragEnd(cardWrapper);
+        });
+
+        it('opacity is 1', async() => {
+          await waitFor(() => expect(card.style.opacity).toEqual('1'));
+        });
+      });
+    });
   });
 });
