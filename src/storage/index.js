df0574c6f0ce10422c564e7b2e2ba0e97aabd129
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-08-16 00:29:36 +0300

    Export/Import tags

--- src/storage/index.js
+++ src/storage/index.js
@@ -3,13 +3,14 @@ import actions from '../store/actions';
 import { initialState as boardInitialState } from '../store/board';
 import { initialState as columnsInitialState } from '../store/columns';
 import { initialState as cardsInitialState } from '../store/cards';
+import { initialState as tagsInitialState } from '../store/tags';
 
-const { importCards, importColumns, importBoard } = actions;
+const { importCards, importColumns, importBoard, importTags } = actions;
 
 export const exportToJSON = () => {
-  const { board, cards, columns } = store.getState();
+  const { board, cards, columns, tags } = store.getState();
   let a = document.createElement('a');
-  const file = new Blob([JSON.stringify({ board, cards, columns })], { type: 'application/json' });
+  const file = new Blob([JSON.stringify({ board, cards, columns, tags })], { type: 'application/json' });
   a.href = URL.createObjectURL(file);
   a.download = 'boardeaux_export.json';
   a.click();
@@ -20,10 +21,12 @@ export const importFromJSON = fileList => {
   const file = fileList[0];
   const reader = new FileReader();
   reader.addEventListener('load', event => {
-    const { board, cards, columns } = JSON.parse(event.target.result);
+    const { board, cards, columns, tags } = JSON.parse(event.target.result);
     store.dispatch(importBoard(boardInitialState));
     store.dispatch(importColumns(columnsInitialState));
     store.dispatch(importCards(cardsInitialState));
+    store.dispatch(importTags(tagsInitialState));
+    store.dispatch(importTags(tags));
     store.dispatch(importCards(cards));
     store.dispatch(importColumns(columns));
     store.dispatch(importBoard(board));
