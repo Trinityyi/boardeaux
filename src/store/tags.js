5b3f537d4632a11c8ab1da88a746ee860fdea327
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-08-16 13:29:10 +0300

    Add colors to tag state

--- src/store/tags.js
+++ src/store/tags.js
@@ -1,9 +1,12 @@
 import { v4 as uuid } from 'uuid';
+import { randomHexColorCode, determineColor } from '../utils';
 
 export const initialState = {};
 
 export const actionTypes = {
   CREATE_TAG: 'CREATE_TAG',
+  SET_TAG_COLORS: 'SET_TAG_COLORS',
+  SET_TAG_NAME: 'SET_TAG_NAME',
   IMPORT_TAGS: 'IMPORT_TAGS'
 
 };
@@ -14,6 +17,23 @@ const reducer = (state = initialState, action) => {
     return {
       ...state,
       [action.id]: action.data
+    };
+  case actionTypes.SET_TAG_COLORS:
+    return {
+      ...state,
+      [action.id]: {
+        ...state[action.id],
+        color: action.color,
+        backgroundColor: action.backgroundColor
+      }
+    };
+  case actionTypes.SET_TAG_NAME:
+    return {
+      ...state,
+      [action.id]: {
+        ...state[action.id],
+        name: action.name
+      }
     };
   case actionTypes.IMPORT_TAGS:
     return action.data;
@@ -26,9 +46,26 @@ export const actions = {
   createTag: data => {
     const id = uuid();
     data.id = id;
+    data.backgroundColor = randomHexColorCode();
+    data.color = determineColor(data.backgroundColor);
     return {
       type: actionTypes.CREATE_TAG,
       data,
+      id
+    };
+  },
+  setTagColors: (id, backgroundColor) => {
+    return {
+      type: actionTypes.SET_TAG_COLORS,
+      backgroundColor,
+      color: determineColor(backgroundColor),
+      id
+    };
+  },
+  setTagName: (id, name) => {
+    return {
+      type: actionTypes.SET_TAG_NAME,
+      name,
       id
     };
   },
